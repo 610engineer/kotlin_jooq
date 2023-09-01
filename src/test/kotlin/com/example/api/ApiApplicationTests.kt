504c2607800d7a7c386160d5538ae5af798aaec5
610engineer <m_muto@jour-1.com>
2023-09-01 23:51:49 +0900

    added test

--- src/test/kotlin/com/example/api/ApiApplicationTests.kt
+++ src/test/kotlin/com/example/api/ApiApplicationTests.kt
@@ -5,9 +5,11 @@ import com.github.database.rider.core.api.dataset.ExpectedDataSet
 import com.github.database.rider.junit5.api.DBRider
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.skyscreamer.jsonassert.Customization
 import org.skyscreamer.jsonassert.JSONAssert
 import org.skyscreamer.jsonassert.JSONCompareMode
 import org.skyscreamer.jsonassert.JSONCompareMode.STRICT
+import org.skyscreamer.jsonassert.comparator.CustomComparator
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -17,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,20 +35,22 @@ class ApiApplicationTests {
 		value = ["datasets/common.yml"],
 		orderBy = ["id"]
 	)
-	fun getAllTest() {
+	fun getById_1Test() {
 
-		val id = 1
+		var id = 1
 
-		val response = mockMvc.perform(
-			get("/api/books/$id")
-			.contentType(MediaType.APPLICATION_JSON)
-		).andReturn().response
-		val actualStatus = response.status
-		val actualResponseBody = response.contentAsString
+		while( id < 7){
+			val response = mockMvc.perform(
+				get("/api/books/$id")
+					.contentType(MediaType.APPLICATION_JSON)
+			).andReturn().response
+			val actualStatus = response.status
+			val actualResponseBody = response.contentAsString
 
-		val expectedStatus = HttpStatus.OK.value()
-		val expectedResponseBody =
-			"""
+			val expectedStatus = HttpStatus.OK.value()
+
+			val expectedResponseBody = when(id){
+				1 -> """
 					{
                      "status": 1,
 						"data": 
@@ -56,13 +61,115 @@ class ApiApplicationTests {
 							}
 					}
                 """.trimIndent()
+				2 ->  """
+					{
+                     "status": 1,
+						"data": 
+							{
+								"id": 2,
+								"title": "title_2",
+								"author": "author_2"
+							}
+					}
+                """.trimIndent()
+				3 ->  """
+					{
+                     "status": 1,
+						"data": 
+							{
+								"id": 3,
+								"title": "title_3",
+								"author": "author_3"
+							}
+					}
+                """.trimIndent()
+				4 ->  """
+					{
+                     "status": 1,
+						"data": 
+							{
+								"id": 4,
+								"title": "title_4",
+								"author": "author_4"
+							}
+					}
+                """.trimIndent()
+				5 ->  """
+					{
+                     "status": 1,
+						"data": 
+							{
+								"id": 5,
+								"title": "title_5",
+								"author": "author_5"
+							}
+					}
+                """.trimIndent()
+
+				else -> """
+					{
+                     "status": 0,
+					 "message":"DATA_NOT_FOUND"
+					}
+                """.trimIndent()
+			}
+
+			Assertions.assertThat(actualStatus).isEqualTo(expectedStatus)
+			JSONAssert.assertEquals(
+				expectedResponseBody,
+				actualResponseBody,
+				JSONCompareMode.STRICT
+			)
+
+			id++
+		}
+	}
+
+	@Test
+	@DataSet("datasets/common.yml")
+	@ExpectedDataSet(
+		value = ["datasets/insert_success.yml"],
+		orderBy = ["id"],
+		ignoreCols = ["id"]
+	)
+	fun insertTest(){
+		val rawRequestBody = """
+			{
+				"title": "title_6",
+				"author": "author_6"
+			}
+		""".trimIndent()
+
+		val response = mockMvc.perform(
+			post("/api/books")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(rawRequestBody)
+		).andReturn().response
+
+		val actualStatus = response.status
+		val actualResponseBody = response.contentAsString
+
+		val expectedStatus =  HttpStatus.OK.value()
+		val expectedResponseBody =
+			"""
+				{
+					"status": 1,
+					"data":{
+						"id" : 6,
+						"title" : "title_6",
+						"author" : "author_6"
+					}
+				}
+			""".trimIndent()
 
 		Assertions.assertThat(actualStatus).isEqualTo(expectedStatus)
 		JSONAssert.assertEquals(
 			expectedResponseBody,
 			actualResponseBody,
-			JSONCompareMode.STRICT
+			CustomComparator(
+				STRICT,
+				Customization("books.id"){_,_ -> true}
+			)
 		)
 	}
-
 }
