799a5aae59c3ccd5115eaad5663896a42ef29bfb
610engineer <m_muto@jour-1.com>
2023-09-03 00:26:41 +0900

    add transaction

--- src/main/kotlin/com/example/api/ApiService.kt
+++ src/main/kotlin/com/example/api/ApiService.kt
@@ -2,27 +2,32 @@ package com.example.api
 
 import com.example.api.model.Book
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ApiService(private val apiRepository: ApiRepository){
+    @Transactional(readOnly = true)
     fun getAllBooks(): String {
         return apiRepository.findAll()
     }
 
+    @Transactional(readOnly = true)
     fun getById(id: Int): String {
         return apiRepository.findById(id)
     }
 
+    @Transactional(readOnly = true)
     fun getByAuthor(author: String): String {
         return apiRepository.findByAuthor(author)
     }
 
+    @Transactional
     fun createBook(book:Book): String {
         return apiRepository.createBook(book)
     }
 
+    @Transactional
     fun updateBook(id: Int, book:Book): String {
         return apiRepository.updateBook(id, book)
     }
-
 }
